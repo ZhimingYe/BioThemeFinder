45ffa282b29c24fa292371adf44e9fbcb1ef9eb8
ZhimingYe <zhiming.ye@qq.com>
2023-01-08 03:21:25 +0800

    Add Self-defining gene set support

--- R/PreviewDupMat.R
+++ R/PreviewDupMat.R
@@ -10,12 +10,14 @@ setMethod("PreviewDupMat",signature(x="BioThemeFinder.ORA"),function(x,...){
                                                                                                                  `GOCC`="#2a9d8f",
                                                                                                                  `GOMF`="#e9c46a",
                                                                                                                  `KEGG`="#f4a261",
-                                                                                                                 `Reactome`="#e76f51")))
+                                                                                                                 `Reactome`="#e76f51",
+                                                                                                               `Self`="#e5989b")))
   colANN<-columnAnnotation(Database=x@Results$Database,Change=x@Results$RegType,col=list(Database=c(`GOBP`="#264653",
                                                                                                       `GOCC`="#2a9d8f",
                                                                                                       `GOMF`="#e9c46a",
                                                                                                       `KEGG`="#f4a261",
-                                                                                                      `Reactome`="#e76f51")))
+                                                                                                      `Reactome`="#e76f51",
+                                                                                                    `Self`="#e5989b")))
   Heatmap(dupmat0,name = "Duplicate Rate",col = colorRampPalette(rev(brewer.pal(n = 11, name = "RdBu")))(100),row_names_gp = gpar(fontsize = 9),column_names_gp = gpar(fontsize = 9),row_names_max_width = max_text_width(rownames(dupmat0),gp = gpar(fontsize = 3)),column_names_max_height = max_text_width(rownames(dupmat0),gp = gpar(fontsize = 3)),right_annotation = rowANN,top_annotation = colANN)
 })
 
@@ -29,7 +31,8 @@ setMethod("PreviewDupMat",signature(x="BioThemeFinder.ORA_FC"),function(x,...){
                                                                                                                  `GOCC`="#2a9d8f",
                                                                                                                  `GOMF`="#e9c46a",
                                                                                                                  `KEGG`="#f4a261",
-                                                                                                                 `Reactome`="#e76f51"),
+                                                                                                                 `Reactome`="#e76f51",
+                                                                                                               `Self`="#e5989b"),
                                                                                                       Change=c(`Favor_UpReg`="#e63946",
                                                                                                                `Favor_DnReg`="#1d3557",
                                                                                                                `UnKnown`="#fefae0")))
@@ -37,7 +40,8 @@ setMethod("PreviewDupMat",signature(x="BioThemeFinder.ORA_FC"),function(x,...){
                                                                                                       `GOCC`="#2a9d8f",
                                                                                                       `GOMF`="#e9c46a",
                                                                                                       `KEGG`="#f4a261",
-                                                                                                      `Reactome`="#e76f51"),
+                                                                                                      `Reactome`="#e76f51",
+                                                                                                    `Self`="#e5989b"),
                                                                                            Change=c(`Favor_UpReg`="#e63946",
                                                                                                     `Favor_DnReg`="#1d3557",
                                                                                                     `UnKnown`="#fefae0")))
@@ -53,7 +57,8 @@ setMethod("PreviewDupMat",signature(x="BioThemeFinder.GSEA"),function(x,...){
                                                                                                                  `GOCC`="#2a9d8f",
                                                                                                                  `GOMF`="#e9c46a",
                                                                                                                  `KEGG`="#f4a261",
-                                                                                                                 `Reactome`="#e76f51"),
+                                                                                                                 `Reactome`="#e76f51",
+                                                                                                        `Self`="#e5989b"),
                                                                                                       Change=c(`Favor_UpReg`="#e63946",
                                                                                                                `Favor_DnReg`="#1d3557",
                                                                                                                `UnKnown`="#fefae0")))
@@ -61,7 +66,8 @@ setMethod("PreviewDupMat",signature(x="BioThemeFinder.GSEA"),function(x,...){
                                                                                                       `GOCC`="#2a9d8f",
                                                                                                       `GOMF`="#e9c46a",
                                                                                                       `KEGG`="#f4a261",
-                                                                                                      `Reactome`="#e76f51"),
+                                                                                                      `Reactome`="#e76f51",
+                                                                                             `Self`="#e5989b"),
                                                                                             `NES` = colorRamp2(c(min(x@Results$NES), max(x@Results$NES)), c("#0e6ba8","#e56b6f"))))
   Heatmap(dupmat0,name = "Duplicate Rate",col = colorRampPalette(rev(brewer.pal(n = 11, name = "RdBu")))(100),row_names_gp = gpar(fontsize = 9),column_names_gp = gpar(fontsize = 9),row_names_max_width = max_text_width(rownames(dupmat0),gp = gpar(fontsize = 3)),column_names_max_height = max_text_width(rownames(dupmat0),gp = gpar(fontsize = 3)),right_annotation = rowANN,top_annotation = colANN)
 })
