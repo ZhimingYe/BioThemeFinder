854e9beb8b4c007b0f68c6bd3ab28e001bf914aa
ZhimingYe <zhiming.ye@qq.com>
2023-01-08 00:03:43 +0800

    useable Generated network function

--- R/NetworkClustering.R
+++ R/NetworkClustering.R
@@ -1,19 +1,18 @@
-ToNetworkDF<-function(x,EdgeCutoff=0.5,nGeneCutOff=3,...){
-
-  ind <- which(upper.tri(df, diag = TRUE), arr.ind = TRUE)
-  NwkDF<-cbind(ind, df[ind])
+ToNetworkDF<-function(x,EdgeCutoff=0.5,...){
+  OriginDF<-x@DupMatrix
+  #name加入DB
+  ind <- which(upper.tri(OriginDF, diag = TRUE), arr.ind = TRUE)
+  NwkDF<-cbind(ind, OriginDF[ind])
   colnames(NwkDF)<-c("from","to","Prob")
-  NwkDF$from<-rownames(x@dup)[NwkDF$from]
-  NwkDF$from<-colnames(x@dup)[NwkDF$to]
+  NwkDF<-as.data.frame(NwkDF)
+  NwkDF$from<-rownames(x@DupMatrix)[NwkDF$from]
+  NwkDF$to<-colnames(x@DupMatrix)[NwkDF$to]
   NwkDF<-NwkDF%>%dplyr::filter(Prob>EdgeCutoff)
-  return(NwkDF)
+  NwkDF<-NwkDF%>%dplyr::filter(from!=to)
+  g <- graph.data.frame(NwkDF, directed = FALSE)
+  return(g)
 }
-a<-c(1,2,3,4,5)
-b<-c(0,5,6,7,8)
-d<-c(0,0,4,6,75)
-e<-c(0,0,0,43,53)
-f<-c(0,0,0,0,13)
-df<-rbind(a,b,d,e,f)
-ind <- which(upper.tri(df, diag = TRUE), arr.ind = TRUE)
-cbind(ind, df[ind])
 
+
+NwkOrigin<-NwkOrigin[,-3]
+g <- graph.data.frame(NwkOrigin, directed = FALSE)
